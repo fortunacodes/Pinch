874e040d9099075932e03e02ad08f78a1e23e14c
jesusfortuna <fortuna.codes@gmail.com>
2023-05-28 12:23:30 +0200

    Initial set up

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jesús  Fortuna on 28/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
