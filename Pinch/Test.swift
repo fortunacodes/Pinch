c987b7d8258cf0789c28920ca2f31c7a8396dedc
jesusfortuna <fortuna.codes@gmail.com>
2023-05-28 15:56:13 +0200

    Drag and Tap gesture have been added

---
+++ Pinch/Test.swift
@@ -0,0 +1,24 @@
+//
+//  Test.swift
+//  Pinch
+//
+//  Created by Jesús  Fortuna on 28/5/23.
+//
+
+import SwiftUI
+
+struct Test: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - BODY
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        Test()
+    }
+}
