4201789b82a8f033af97cc996f2f7b7556e99595
jesusfortuna <fortuna.codes@gmail.com>
2023-05-29 20:15:41 +0200

    Drawer has been added

--- Pinch/Pinch/ContentView.swift
+++ Pinch/Pinch/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     
     // MARK: - FUNCTION
     func resetImageState(){
@@ -71,7 +72,7 @@ struct ContentView: View {
                                         imageScale = value
                                     } else if imageScale > 5{
                                         imageScale = 5
-                                    } 
+                                    }
                                 }
                             }
                             .onEnded({ _ in
@@ -145,6 +146,34 @@ struct ContentView: View {
                 }
                     .padding(.bottom, 30)
                 ,alignment: .bottom
+            )
+            .overlay(
+                HStack(spacing: 12){
+                    //MARK: - DRAWER HANDLE
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        }
+                        
+                    //MARK: - THUMBNAILS
+                    Spacer()
+                    
+                }//: DRAWER
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20 : 215)
+                ,alignment: .topTrailing
             )
         }//: NAVIGATION
         .navigationViewStyle(.stack)
